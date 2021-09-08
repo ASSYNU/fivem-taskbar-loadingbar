d9c1d3f498715f8d9638697b92f628f38cfbe64d
Assynu <66019090+ASSYNU@users.noreply.github.com>
2021-09-08 22:16:31 +0200

    Add files via upload

---
+++ Assynu_Taskbar/client/cl_demo.lua
@@ -0,0 +1,3 @@
+RegisterCommand('taskbar_test', function()
+  exports["Assynu_Taskbar"]:taskBar(20000, 20)
+end)
---
+++ Assynu_Taskbar/html/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  var documentWidth = document.documentElement.clientWidth;
+  var documentHeight = document.documentElement.clientHeight;
+  var curTask = 0;
+  var processed = [];
+  var percent = 0;
+
+  document.onkeydown = function (data) {
+    if (data.which == 69) {
+      closeMain();
+      $.post(
+        'https://Assynu_Taskbar/taskEnd',
+        JSON.stringify({ taskResult: percent })
+      );
+    }
+  };
+
+  function openMain() {
+    $('.divwrap').fadeIn(10);
+  }
+
+  function closeMain() {
+    $('.divwrap').css('display', 'none');
+  }
+
+  window.addEventListener('message', function (event) {
+    var item = event.data;
+    if (item.runProgress === true) {
+      percent = 0;
+      openMain();
+      $('#progress-bar').css('width', '0%');
+      $('.skillprogress').css('left', item.chance + '%');
+      $('.skillprogress').css('width', item.skillGap + '%');
+    }
+
+    if (item.runUpdate === true) {
+      percent = item.Length;
+      $('#progress-bar').css('width', item.Length + '%');
+
+      if (
+        item.Length < item.chance + item.skillGap &&
+        item.Length > item.chance
+      ) {
+        $('.skillprogress').css('background-color', 'rgba(120,50,50,0.9)');
+      } else {
+        $('.skillprogress').css('background-color', 'rgba(255,250,250,0.4)');
+      }
+    }
+
+    if (item.closeFail === true) {
+      closeMain();
+      $.post(
+        'https://Assynu_Taskbar/taskCancel',
+        JSON.stringify({ tasknum: curTask })
+      );
+    }
+
+    if (item.closeProgress === true) {
+      closeMain();
+    }
+  });
+});
